75ea6a3c986318ed1095e4641b40a0eb9bb87113
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-11-10 13:38:16 -0600

    Removed weird imports, put everything in a main function

--- download.py
+++ download.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 import sys
+
 from downloader import Downloader
-#import imp
-#from settings import program_path
-#load = imp.load_source('Downloader', program_path+'downloader.py')
-#Downloader = load.Downloader
-argv = sys.argv[1:]
 
-if argv:
-        search_term = argv[0]
-        desired_item_name = argv[1]
-else:
+def main(args):
+    if args:
+        search_term = args[0]
+        desired_item_name = args[1]
+    else:
         search_term = raw_input('Search Term:')
-        desired_item_name = raw_input('Desired Result:') 
+        desired_item_name = raw_input('Desired Result:')
+    downloader = Downloader()
+    downloader.download(search_term, desired_item_name)
 
-downloader = Downloader()
-downloader.download(search_term, desired_item_name)
+if __name__ == "__main__":
+    main(sys.argv[1:])
