916381a98113dbeed8b93936d096e573fe9f0928
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-11-26 17:51:38 -0500

    Added exception and exception handling, renamed variables and functions to be more descriptive

--- downloader.py
+++ downloader.py
@@ -1,18 +1,20 @@
-import urllib
+import urllib2
 import re
 import os
 import subprocess
 import imp
 
 from BeautifulSoup import BeautifulSoup
+
 from settings import DOWNLOADS_FOLDER, PROGRAM_PATH
 from utils import remove_html_tags, remove_entities
+from errors import DownloaderError
 
-class Downloader:
+class Downloader(object):
     def __init__(self):
         #identify the trackers we can use that exist in the trackers folder
         self._trackers = self._get_trackers()
-    
+
     def _get_trackers(self):
         """Loads tracker files and populates a dict with them"""
         trackers_path = os.path.join(PROGRAM_PATH, 'trackers')
@@ -21,89 +23,68 @@ class Downloader:
         trackers = []
         for tracker in tracker_files:
             tracker_name = tracker.replace('.py', '')
-            trackers.append(tracker_name)           
+            trackers.append(tracker_name)
         return trackers
-        
-    def download(self, search_term, desired_item_name):     
+
+    def download(self, search_term, desired_item_name):
         """Tries to download something with the desired_item_name from the torrentz results after searching for search_term"""
-        good = True
-        #TODO: make errors raise exceptions, then handle them
-        error_step = ''       
         #TODO: make this a general search call that can take any search site
         #search_results is BeautifulSoup of torrentz.com
-        search_results = self._torrentz_search(search_term)        
-        #intial_results is a dict of link titles and urls that lead to the 'choose a tracker' page on torrentz
-        initial_results =  self._make_initial_results_dict(search_results)
-        if initial_results == {}:
-            good = False
-            error_step = 'make_inital_results_dict'        
-        if good:
-            #picks out a link we can use 
-            desired_link = self._desired_link_from_initial(initial_results, desired_item_name)
-            if not desired_link:
-                good = False
-                error_step = 'desired_link_from_initial'        
-        if good:
-            #results is a dict of tracker names and url's
-            results = self._make_secondary_results_dict(desired_link)
-            if results != {}:
-                trackers = results.keys()
-            else:
-                good = False
-                error_step = 'make_secondary_results_dict'        
-        if good:
-            #file_path is the location of the .torrent file or -1 if it didn't work
-            file_path = self._download_torrent_file(search_term, desired_item_name, results)
-            if not file_path:
-                good = False
-                error_step = 'download_torrent_file'        
-        if good:
+        search_results = self._torrentz_search(search_term)
+        try:
+            #results from tracker meta-search
+            parsed_general_results =  self._parse_general_search(search_results)
+            #identifies the item we want to download
+            desired_link = self._general_result_link(parsed_general_results, desired_item_name)
+            #identifies trackers with desired item
+            tracker_results = self._find_trackers(desired_link)
+            #downloads .torrent file of item to file_path
+            file_path = self._download_torrent_file(search_term, desired_item_name, tracker_results)
             self._open_torrent(file_path)
-        else:
-            print 'unable to find a valid torrent.'
-            print 'problem in: %s' % error_step        
+        except DownloaderError as error:
+           print 'Unable to download -- %s' % str(error)
         return 0
 
-    def _download_torrent_file(self, search_term, desired_item_name, results, tracker_number=0):
-        #TODO: raise exceptions here if we can't find anything, rather then returning None
-        file_path = None
-        for tracker, tracker_url in results.items():
-            base_file_path = DOWNLOADS_FOLDER
-            file_path = base_file_path + desired_item_name.replace(' ', '') + '.torrent'
-            print 'downloading from ' + tracker
-            #start_url is the torrent's page on the tracker's site
-            tracker_file = tracker + ".py"
-            tracker_path = os.path.join(PROGRAM_PATH, 'trackers', tracker_file)
-            source = imp.load_source('find_url', tracker_path)
-            find_url = source.find_url          
-            url = find_url(tracker_url, PROGRAM_PATH)         
-            if url:
-                result = os.system('wget -O "%s" "%s" -t 2 -T 5' % (file_path, url))
-                if result != 0:
-                    #recursive call if wget is unable to download the .torrent file from the tracker
-                    #increments tracker_number so we call the next available tracker
-                    file_path = self._download_torrent_file(search_term, desired_item_name, tracker_number+1)
-        return file_path
-    
+    def _download_torrent_file(self, search_term, desired_item_name, tracker_results):
+        ''''Uses wget to download a .torrent file'''
+        downloaded = False
+        for tracker_name, tracker_url in tracker_results.items():
+            if not downloaded:
+                base_file_path = DOWNLOADS_FOLDER
+                file_path = base_file_path + desired_item_name.replace(' ', '') + '.torrent'
+                print 'Downloading from', '%s...' % tracker_name
+                tracker_file = tracker_name + ".py"
+                tracker_path = os.path.join(PROGRAM_PATH, 'trackers', tracker_file)
+                source = imp.load_source('find_url', tracker_path)
+                find_url = source.find_url
+                #url is the actual torrent's url on the tracker's site
+                url = find_url(tracker_url, PROGRAM_PATH)
+                if url:
+                    wget_result = os.system('wget -O "%s" "%s" -t 2 -T 5' % (file_path, url))
+                    if wget_result == 0:
+                        downloaded = True
+        if downloaded:
+            return file_path
+        else:
+            raise DownloaderError('Unable to download from any tracker')
+
     def _torrentz_search(self, search_term):
         """Searches for search_term on torrentz.com and returns the soup that results."""
-        print 'getting torrentz results'        
         search_term = search_term.lower()
         split_term = search_term.split()
         url = 'http://www.torrentz.com/feed?q='
         for word in split_term:
             url += word + '+'
         url = url[:-1]
-        sock = urllib.urlopen(url)
+        sock = urllib2.urlopen(url)
         html = sock.read()
-        sock.close()       
+        sock.close()
         soup = BeautifulSoup(html)
-        print 'done'
         return soup
-        
-    def _make_initial_results_dict(self, search_results):
-        print 'making initial_results_dict'
-        initial_result_dict = {}     
+
+    def _parse_general_search(self, search_results):
+        '''Finds the item to download from meta-search results'''
+        parsed_results = {}
         for item in search_results('item'):
             description = str(item.description)
             seeds_index = description.index('Seeds') + 7
@@ -111,7 +92,7 @@ class Downloader:
             space_index = cut_description.index(' ')
             seeds = cut_description[:space_index]
             seeds = seeds.replace(",", "")
-            seeds = int(seeds)    
+            seeds = int(seeds)
             if 'flac' not in str(item.category).lower() and 'wma' not in str(item.category).lower() and seeds >= 5:
                 title = item.title
                 title = remove_html_tags(str(title))
@@ -119,46 +100,49 @@ class Downloader:
                 #guid is the url of the 'choose a tracker' page on torrentz
                 guid = item.guid
                 guid = remove_html_tags(str(guid))
-                initial_result_dict[title] = guid        
-        print 'done'
-        return initial_result_dict
+                parsed_results[title] = guid
+        if parsed_results == {}:
+            raise DownloaderError('No valid results for search term')
+        return parsed_results
 
-    def _desired_link_from_initial(self, initial_results_dict, desired_item_name):
+    def _general_result_link(self, parsed_general_results, desired_item_name):
         """Returns a url for the 'choose a tracker' page on torrentz"""
-        for result in initial_results_dict:
-            if desired_item_name.lower() in result.lower():
-                return initial_results_dict[result]
-        return None
-        
-    def _make_secondary_results_dict(self, secondary_results_link):
-        print 'making secondary results dict'
-        try:
-            sock = urllib.urlopen(secondary_results_link)
-            html = sock.read()
-            sock.close()
-        except IOError:
-            return {}
-            
-        soup = BeautifulSoup(html)      
-        #all possible links on the page
-        possibilities = soup.findAll('a')
-        possibilities_dict = {}
-        for possibility in possibilities:
-            tracker = remove_html_tags(str(possibility)).split()
-            if tracker:
-                #tracker[0] is the name of the tracker
-                tracker = tracker[0].replace('.com', '')
-                if tracker in self._trackers:
-                    #link is "href="http://whatever.com"
-                    link = str(possibility).split()[1]
-                    first_quote = link.index('"') + 1
-                    second_quote = link.index('"', first_quote)
-                    link = link[first_quote:second_quote]
-                    #now link is just url of tracker
-                    possibilities_dict[tracker] = link      
-        print 'done'
-        return possibilities_dict
-              
+        for name, link in parsed_general_results.items():
+            if desired_item_name.lower() in name.lower():
+                return link
+        raise DownloaderError('Desired item not in search results')
+
+    def _find_trackers(self, secondary_results_link, attempts=0):
+        '''Identifies trackers that have the file that we can download from'''
+        found_trackers = {}
+        if attempts < 3:
+            try:
+                sock = urllib2.urlopen(secondary_results_link)
+                html = sock.read()
+                sock.close()
+            except urllib2.URLError:
+                attempts += 1
+                found_trackers = self._find_trackers(secondary_results_link, attempts)
+            soup = BeautifulSoup(html)
+            #all possible links on the page
+            possible_trackers = soup.findAll('a')
+            for possible_tracker in possible_trackers:
+                tracker = remove_html_tags(str(possible_tracker)).split()
+                if tracker:
+                    #tracker[0] is the name of the tracker
+                    tracker = tracker[0].replace('.com', '')
+                    if tracker in self._trackers:
+                        #link is "href="http://whatever.com"
+                        link = str(possible_tracker).split()[1]
+                        first_quote = link.index('"') + 1
+                        second_quote = link.index('"', first_quote)
+                        link = link[first_quote:second_quote]
+                        #now link is just url of tracker
+                        found_trackers[tracker] = link
+        if found_trackers == {}:
+            raise DownloaderError('No known trackers')
+        return found_trackers
+
     def _open_torrent(self, file_path):
         if os.name == 'posix':
             subprocess.call(('open', file_path))
