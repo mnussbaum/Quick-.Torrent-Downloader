8369647296358fc92c6cb6c0410e3d0defc29c61
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-12-16 17:46:04 -0500

    all tracker objects have same name now

--- code/trackers/base_tracker.py
+++ code/trackers/base_tracker.py
@@ -1,13 +1,9 @@
-import sys
-
-TRACKER_NAME = 'BaseTracker'
-
 class BaseTracker(object):
     '''Basic tracker object to be subclassed. Takes the url
     of a torrent page from the tracker and returns the url of
     the downloadable .torrent file'''
     def __init__(self):
-        pass
+       self.name = "BaseTracker"
 
     def extract_download_url(self, url):
         return url
