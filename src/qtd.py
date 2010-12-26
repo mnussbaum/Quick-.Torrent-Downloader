6062a97824aeb0efbec3738f812c6cb71b0b56d3
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-12-26 15:41:26 -0500

    added command line arg parsing

--- src/qtd.py
+++ src/qtd.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
+import getopt
 import sys
 
 from downloader import Downloader
 
-def main(args):
-    if args:
-        search_term = args[0]
-        desired_item_name = args[1]
-    else:
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "s:r:")
+    except getopt.GetoptError:
+        print "Please use -s to specify a search term, and -r to specify the desired result name"
+        sys.exit(1)
+    search_term = None
+    desired_item_name = None
+    for opt, arg in opts:
+        if opt == '-s':
+            search_term = arg
+        elif opt == '-r':
+            desired_item_name = arg
+    if not desired_item_name or not search_term:
         search_term = raw_input('Search Term: ')
         desired_item_name = raw_input('Item to Find: ')
     downloader = Downloader()
@@ -15,4 +25,4 @@ def main(args):
     return 0
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
