1759b764cb5d84e39ad1db9775928d46e976d9ad
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-12-31 19:06:28 -0500

    added error handling for file writing

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,23 @@
 import re
 from htmlentitydefs import name2codepoint
 
+from errors import DownloaderError
+
 def get_input(message):
     input = raw_input(message)
     return input
 
 def write_file(file_path, data):
-    out_file = open(file_path, 'w')
-    out_file.write(data)
-    out_file.close()
+    try:
+        out_file = open(file_path, 'w')
+        out_file.write(data)
+        out_file.close()
+    except IOError as error:
+        if '13' in error:
+            raise DownloaderError('Need higher permissions to write' +
+              'settings')
+        else:
+            raise DownloaderError('File error')
 
 def remove_html_tags(data):
     p = re.compile(r'<.*?>')
