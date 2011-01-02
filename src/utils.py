8397575c7ace9d6ea10abf28cde46ed5bf27bc3f
Michael Nussbaum <michaelnussbaum08@gmail.com>
2011-01-02 16:21:51 -0600

    removed now unused function

--- src/utils.py
+++ src/utils.py
@@ -3,24 +3,15 @@ from htmlentitydefs import name2codepoint
 
 from errors import DownloaderError
 
-def get_input(message):
-    input = raw_input(message)
-    return input
-
 def write_file(file_path, data):
-    try:
-        out_file = open(file_path, 'w')
-        out_file.write(data)
-        out_file.close()
-    except IOError as error:
-        msg = 'Error, probably need permissions to write settings,' + \
-          ' please run as root'
-        print msg
+    out_file = open(file_path, 'w')
+    out_file.write(data)
+    out_file.close()
 
 def remove_html_tags(data):
     p = re.compile(r'<.*?>')
     return p.sub('', data)
 
 def remove_entities(word):
-	word = word.replace("&#039;", "'")
-	return word
+    word = word.replace("&#039;", "'")
+    return word
